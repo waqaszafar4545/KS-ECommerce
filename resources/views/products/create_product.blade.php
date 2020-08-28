a14c48e4481ea46efd1080ba527ba2867ab985a2
Mudassar-Hashmi <mudassarhussain08@gmail.com>
2020-08-28 04:52:37 -0700

    adding

--- resources/views/products/create_product.blade.php
+++ resources/views/products/create_product.blade.php
@@ -1,1 +1,26 @@
-<h1>Test for create product</h1>
+<!-- <h1>Test for create product</h1> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        div{
+            border: 1px solid black;
+            width:50px;
+        height:100px;
+        }
+        .div3{
+            position:absolute;
+            top:0;
+            right:0;
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <div class="div1">Div</div>
+    <div class="div2">Div</div>
+    <div class="div3">Div</div>
+</body>
+</html>
